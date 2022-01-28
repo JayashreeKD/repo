a0d1f3a75ab7201b6a05f05f36cc24a116c6a002
Mohanrajvilva <mohanrajvilva@gmail.com>
2022-01-28 23:16:20 +0530

    commit

--- src/test/java/createcontact/CreatecontactTest.java
+++ src/test/java/createcontact/CreatecontactTest.java
@@ -3,21 +3,6 @@ package createcontact;
 import org.testng.annotations.Test;
 
 public class CreatecontactTest {
-	@Test
-	public void createcontacttest()
-	{
-		System.out.println("Contact created sucessflly");
-	}
-	@Test
-	public void modifycontacttest() {
-		System.out.println("contact modigied sucessfully");
-	}
-	
-	@Test
-	public void deletecontacttest()
-	{
-		System.out.println("delete the contact");
-	}
 	@Test(groups="smoketesting")
 	public void contacttest()
 	{
--- src/test/java/createorg/OrgTest.java
+++ src/test/java/createorg/OrgTest.java
@@ -5,13 +5,6 @@ import org.testng.annotations.Test;
 
 public class OrgTest {
 	
-	@Test
-	public void createOrgtest(){
-		System.out.println("Organisation created sucessfully");
-	}
-	public void modifycontacttest() {
-		System.out.println("Organisation modifies sucessfully");
-	}
 	
 		
 	@Test(groups="smoketesting")
