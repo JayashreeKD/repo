0568fdaa9e34cfb51f69a8922a436a04c2e08b5c
Mohanrajvilva <mohanrajvilva@gmail.com>
2022-01-28 22:06:20 +0530

    mohanraj commit

---
+++ src/test/java/createcontact/CreatecontactTest.java
@@ -0,0 +1,15 @@
+package createcontact;
+
+import org.testng.annotations.Test;
+
+public class CreatecontactTest {
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("Contact created sucessflly");
+	}
+	@Test
+	public void modifycontact() {
+		System.out.println("contact modigied sucessfully");
+	}
+}
---
+++ src/test/java/createorg/OrgTest.java
@@ -0,0 +1,15 @@
+package createorg;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgtest(){
+		System.out.println("Organisation created sucessfully");
+	}
+	public void modifycontacttest() {
+		System.out.println("Organisation modifies sucessfully");
+	}
+
+}
